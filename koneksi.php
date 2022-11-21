d0d1bd33778d6b117822f38ee09ea83049694f57
thoriqlukman <thoriqlh123@gmail.com>
2022-11-21 07:35:35 +0700

    thoriq

--- koneksi.php
+++ koneksi.php
@@ -2,6 +2,7 @@
     // Koneksi ke database mysql
     $koneksi = mysqli_connect("localhost", "root", "", "db_tailor");
     // Cek koneksi
+    //asasasadvhwvjbwkdj qlknwq
     if (!$koneksi) {
         die("Koneksi gagal: " . mysqli_connect_error());
     }
