b78f0fa94d134effba417b5d5f510fe37a65d9ff
gitfkrid <e41210477@student.polije.ac.id>
2022-12-04 11:47:29 +0700

    fikri

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php 
     //make connection mysql
     $koneksi = mysqli_connect("localhost","root","","db_tailor");
-    //check connection
+    //check connection msyql
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
